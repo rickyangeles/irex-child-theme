1280d99a8761ce3b77cd19ddc65b045d7eb8a5a8
Ricky Angeles <ricky@improveandgrow.com>
2019-12-26 17:48:14 -0500

    adding industry taxonomy

--- library/cpt.php
+++ library/cpt.php
@@ -501,4 +501,45 @@ function add_custom_taxonomy() {
 	register_taxonomy( 'location_tax', array('location'), array( 'hierarchical' => true, 'label' => 'Location' ) );
 	register_taxonomy( 'service_tax', array('location'), array( 'hierarchical' => false, 'label' => 'Services', 'meta_box_cb' => true ) );
 }
+
+// Register Industry Taxonomy
+function industry_tax() {
+
+	$labels = array(
+		'name'                       => _x( 'Industries', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Industry', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Industry', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+		'show_in_rest'               => true,
+		'rest_base'                  => 'industry_tax',
+	);
+	register_taxonomy( 'industry_tax', array( 'post', 'page', 'service', 'project_gallery', 'testimonial' ), $args );
+
+}
+add_action( 'init', 'industry_tax', 0 );
  ?>
