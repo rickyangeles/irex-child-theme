b3eda130592313bac88d3e60a1791288c3d2b141
Ricky Angeles <ricky@improveandgrow.com>
2019-12-27 09:41:58 -0500

    updates to the industry single page

--- single-industry.php
+++ single-industry.php
@@ -15,6 +15,7 @@ get_header(); ?>
     $industryCTAcontent = get_field('industry_cta_text') ? get_field('industry_cta_text') : get_field('industry_cta_content', 'options');
     $industryCTAbtn     = get_field('industry_cta_button') ? get_field('industry_cta_button') : get_field('industry_cta_button', 'options');
     $pID                = get_the_ID();
+    $sub = get_field('subsidiary_site', 'options');
 ?>
 <!-- Page Header -->
 <div class="container-fluid page-header">
@@ -143,7 +144,7 @@ get_header(); ?>
     ) );
 ?>
 
-<?php if ( $project_query ) :?>
+<?php if ( $project_query->have_posts() ) : ?>
     <div class="container home-featured-projects">
         <div class="row">
             <h2 class="title">Featured Projects</h2>
@@ -180,71 +181,73 @@ get_header(); ?>
 <?php endif; ?>
 
 <!-- Contractor -->
-<div class="container page-contractor">
-    <h3 class="service-title"><?php the_title(); ?> Providers</h3>
-        <?php
-            $meta = get_post_meta($post->ID, 'dt_connection_map', false);
-            $items = array();
-            foreach($meta as $k => $v) {
-                foreach ($v as $kk => $vv ) {
-                    if ($kk == 'external') {
-                        foreach($vv as $kkk => $vvv) {
-                            $items[] = $kkk;
+<?php if (!$sub) : ?>
+    <div class="container page-contractor">
+        <h3 class="service-title"><?php the_title(); ?> Providers</h3>
+            <?php
+                $meta = get_post_meta($post->ID, 'dt_connection_map', false);
+                $items = array();
+                foreach($meta as $k => $v) {
+                    foreach ($v as $kk => $vv ) {
+                        if ($kk == 'external') {
+                            foreach($vv as $kkk => $vvv) {
+                                $items[] = $kkk;
+                            }
                         }
                     }
                 }
-            }
-            $query = new WP_Query(array(
-                'post_type' => 'dt_ext_connection',
-                'post__in' => $items,
-                'order'    => 'ASC'
-            ));
-        ?>
-        <?php $counter = 0; ?>
-            <div class="row folding-menu">
-            <?php while ($query->have_posts()) : ?>
-                <?php $query->the_post(); ?>
-                <?php
-                    // if ($counter % 4 == 0) :
-                    //     echo $counter > 0 ? '</div></div>' : ''; // close div if it's not the first
-                    //     echo '<div class="home-contractor-row container"><div class="row">';
-                    // endif; ?>
+                $query = new WP_Query(array(
+                    'post_type' => 'dt_ext_connection',
+                    'post__in' => $items,
+                    'order'    => 'ASC'
+                ));
+            ?>
+            <?php $counter = 0; ?>
+                <div class="row folding-menu">
+                <?php while ($query->have_posts()) : ?>
+                    <?php $query->the_post(); ?>
                     <?php
-                    $remove = array("/wp-json", "http://");
-                    $url = get_post_meta(get_the_ID(), 'dt_external_connection_url', true);
-                    $cleanUrl = str_replace($remove,'', $url);
-                    $siteURL = str_replace('/wp-json', '', $url);
-                    $title = get_the_title(get_the_ID());
-                    $services = $url . "/wp/v2/service/";
-                    $locations = $url . "/wp/v2/location/";
-                    $logo = $url . "/acf/v3/options/options/header_logo";
-                ?>
-                <div class="menu-item col-md-3 single-sub">
-                  <a href="#">
-                    <img class="sub-title" data-url="<?php echo $cleanUrl;?>" src="<?php echo get_logo_rest($logo); ?>"/>
-                  </a>
-                  <div class="folding-content single-sub-info container-fluid">
-                      <div class="row">
-                          <div class="col-md-6">
-                              <h2><?php echo $title; ?></h2>
-                              website: www.<?php echo $cleanUrl; ?>
-                              <p class="description">description goes here</p>
-                              <a href="<?php echo $siteURL; ?>" class="btn btn-primary">Visit Site</a>
-                          </div>
-                          <div class="col-md-3">
-                              <h4>Services</h4>
-                              <?php echo get_services_rest($services); ?>
-                          </div>
-                          <div class="col-md-3">
-                              <h4>Locations</h4>
-                              <?php echo get_locations_rest($locations, $title); ?>
+                        // if ($counter % 4 == 0) :
+                        //     echo $counter > 0 ? '</div></div>' : ''; // close div if it's not the first
+                        //     echo '<div class="home-contractor-row container"><div class="row">';
+                        // endif; ?>
+                        <?php
+                        $remove = array("/wp-json", "http://");
+                        $url = get_post_meta(get_the_ID(), 'dt_external_connection_url', true);
+                        $cleanUrl = str_replace($remove,'', $url);
+                        $siteURL = str_replace('/wp-json', '', $url);
+                        $title = get_the_title(get_the_ID());
+                        $services = $url . "/wp/v2/service/";
+                        $locations = $url . "/wp/v2/location/";
+                        $logo = $url . "/acf/v3/options/options/header_logo";
+                    ?>
+                    <div class="menu-item col-md-3 single-sub">
+                      <a href="#">
+                        <img class="sub-title" data-url="<?php echo $cleanUrl;?>" src="<?php echo get_logo_rest($logo); ?>"/>
+                      </a>
+                      <div class="folding-content single-sub-info container-fluid">
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <h2><?php echo $title; ?></h2>
+                                  website: www.<?php echo $cleanUrl; ?>
+                                  <p class="description">description goes here</p>
+                                  <a href="<?php echo $siteURL; ?>" class="btn btn-primary">Visit Site</a>
+                              </div>
+                              <div class="col-md-3">
+                                  <h4>Services</h4>
+                                  <?php echo get_services_rest($services); ?>
+                              </div>
+                              <div class="col-md-3">
+                                  <h4>Locations</h4>
+                                  <?php echo get_locations_rest($locations, $title); ?>
+                              </div>
                           </div>
                       </div>
                   </div>
-              </div>
-        <?php $counter++; endwhile; ?>
+            <?php $counter++; endwhile; ?>
+        </div>
+            <?php wp_reset_postdata(); ?>
     </div>
-        <?php wp_reset_postdata(); ?>
-    </div>
+<?php endif; ?>
 </div>
 <?php get_footer(); ?>
