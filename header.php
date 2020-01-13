bed6fd2123c789ccbae29c57d6d255988feca0ad
Ricky Angeles <ricky@improveandgrow.com>
2020-01-13 08:05:20 -0500

    adding favicon

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<link rel="icon" type="image/png" href="favicon.png" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
