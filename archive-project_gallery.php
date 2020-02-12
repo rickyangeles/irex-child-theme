ae3ba5bd9df4d312d551b683c98047b17f85090c
Ricky Angeles <ricky@improveandgrow.com>
2020-02-12 15:27:44 -0500

    fixes to the project archive page

--- archive-project_gallery.php
+++ archive-project_gallery.php
@@ -33,16 +33,20 @@ get_header(); ?>
         <?php while ( have_posts() ) : the_post(); ?>
             <div class="col-md-6 single-project d-flex align-items-center">
                 <div class="sfp-left">
+                    <a href="<?php the_permalink(); ?>">
                     <h5><?php the_title(); ?></h5>
                     <p><?php echo project_excerpt(15, $post->ID); ?></p>
+                    </a>
                 </div>
                 <div class="sfp-right d-flex align-items-center">
+                    <a href="<?php the_permalink(); ?>">
                     <?php if ( has_post_thumbnail()): ?>
                         <?php the_post_thumbnail('featured-project'); ?>
                     <?php else : ?>
                         <img src="https://via.placeholder.com/300">
                     <?php endif; ?>
-                    <span><a class="read-more btn btn-sm" href="<?php the_permalink(); ?>">Read More</a></span>
+                    <div class="d-flex justify-content-center align-items-center"><span class="read-more btn btn-sm">Read More</span></div>
+                </a>
                 </div>
             </div>
         <?php endwhile; ?>
